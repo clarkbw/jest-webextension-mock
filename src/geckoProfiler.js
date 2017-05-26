c7de77d6c30cc2d468057720c2601931884b929b
Bryan Clark <clarkbw@gmail.com>
2017-05-25 17:17:40 -0700

    Update geckoProfiler with promises

--- src/geckoProfiler.js
+++ src/geckoProfiler.js
@@ -1,12 +1,12 @@
 export const geckoProfiler = {
-  stop: jest.fn(),
-  start: jest.fn(),
-  pause: jest.fn(),
-  resume: jest.fn(),
-  getProfile: jest.fn(),
-  getProfileAsArrayBuffer: jest.fn(),
+  stop: jest.fn(() => Promise.resolve()),
+  start: jest.fn(() => Promise.resolve()),
+  pause: jest.fn(() => Promise.resolve()),
+  resume: jest.fn(() => Promise.resolve()),
+  getProfile: jest.fn(() => Promise.resolve()),
+  getProfileAsArrayBuffer: jest.fn(() => Promise.resolve()),
   getSymbols: jest.fn(function(debugName, breakpadId) {
-    return this;
+    return Promise.resolve();
   }),
   onRunning: {
     addListener: jest.fn(),
