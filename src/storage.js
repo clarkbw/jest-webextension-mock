69dd834917fa1326115d77c8685029df88697a7d
AllenFang <ayu780129@hotmail.com>
2017-12-03 15:04:24 +0800

    refine browser.storage API

--- src/storage.js
+++ src/storage.js
@@ -1,28 +1,45 @@
+function resolveKey(key) {
+  if (typeof key === 'string') {
+    const result = {};
+    result[key] = '';
+    return result;
+  } else if (Array.isArray(key)) {
+    return key.reduce((acc, curr) => {
+      acc[curr] = '';
+      return acc;
+    }, {});
+  } else if (typeof key === 'object') {
+    return key;
+  }
+  throw new Error('Wrong key given');
+}
+
 export const storage = {
   sync: {
     get: jest.fn((id, cb) => {
+      const result = resolveKey(id);
       if (cb !== undefined) {
-        return cb({ id });
+        return cb(result);
       }
-      return Promise.resolve({ id });
+      return Promise.resolve(result);
     }),
     getBytesInUse: jest.fn((id, cb) => {
       if (cb !== undefined) {
-        return cb({ id });
+        return cb(0);
       }
-      return Promise.resolve({ id });
+      return Promise.resolve(0);
     }),
     set: jest.fn((id, cb) => {
       if (cb !== undefined) {
-        return cb({ id });
+        return cb();
       }
-      return Promise.resolve({ id });
+      return Promise.resolve();
     }),
     remove: jest.fn((id, cb) => {
       if (cb !== undefined) {
-        return cb({ id });
+        return cb();
       }
-      return Promise.resolve({ id });
+      return Promise.resolve();
     }),
     clear: jest.fn(cb => {
       if (cb !== undefined) {
@@ -33,28 +50,29 @@ export const storage = {
   },
   local: {
     get: jest.fn((id, cb) => {
+      const result = resolveKey(id);
       if (cb !== undefined) {
-        return cb({ id });
+        return cb(result);
       }
-      return Promise.resolve({ id });
+      return Promise.resolve(result);
     }),
     getBytesInUse: jest.fn((id, cb) => {
       if (cb !== undefined) {
-        return cb({ id });
+        return cb(0);
       }
-      return Promise.resolve({ id });
+      return Promise.resolve(0);
     }),
     set: jest.fn((id, cb) => {
       if (cb !== undefined) {
-        return cb({ id });
+        return cb();
       }
-      return Promise.resolve({ id });
+      return Promise.resolve();
     }),
     remove: jest.fn((id, cb) => {
       if (cb !== undefined) {
-        return cb({ id });
+        return cb();
       }
-      return Promise.resolve({ id });
+      return Promise.resolve();
     }),
     clear: jest.fn(cb => {
       if (cb !== undefined) {
@@ -65,28 +83,29 @@ export const storage = {
   },
   managed: {
     get: jest.fn((id, cb) => {
+      const result = resolveKey(id);
       if (cb !== undefined) {
-        return cb({ id });
+        return cb(result);
       }
-      return Promise.resolve({ id });
+      return Promise.resolve(result);
     }),
     getBytesInUse: jest.fn((id, cb) => {
       if (cb !== undefined) {
-        return cb({ id });
+        return cb(0);
       }
-      return Promise.resolve({ id });
+      return Promise.resolve(0);
     }),
     set: jest.fn((id, cb) => {
       if (cb !== undefined) {
-        return cb({ id });
+        return cb();
       }
-      return Promise.resolve({ id });
+      return Promise.resolve();
     }),
     remove: jest.fn((id, cb) => {
       if (cb !== undefined) {
-        return cb({ id });
+        return cb();
       }
-      return Promise.resolve({ id });
+      return Promise.resolve();
     }),
     clear: jest.fn(cb => {
       if (cb !== undefined) {
