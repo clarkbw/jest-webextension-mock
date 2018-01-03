ee273f312dd66480e8c90c1178266b2bf1d18317
Hugo Alliaume <kocal@live.fr>
2018-01-03 21:40:57 +0100

    Notification API: implement « create » and its tests

--- src/notifications.js
+++ src/notifications.js
@@ -1,1 +1,24 @@
-export const notifications = {};
+export const notifications = {
+  create: jest.fn((notificationId, options, cb) => {
+    if (arguments.length === 1) {
+      // only options is specified
+      options = notificationId;
+      notificationId = undefined;
+    } else if (arguments.length === 2 && typeof notificationId === 'object') {
+      // options & cb are specified
+      options = notificationId;
+      cb = options;
+      notificationId = undefined;
+    }
+
+    if (typeof notificationId !== 'string') {
+      notificationId = 'generated-id';
+    }
+
+    if (cb !== undefined) {
+      return cb(notificationId);
+    }
+
+    return Promise.resolve(notificationId);
+  }),
+};
