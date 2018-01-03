94c0d88584a64285208be1b20d2f8e54ecf54c82
Hugo Alliaume <kocal@live.fr>
2018-01-03 22:11:04 +0100

    Notification API: simplify 10 times « create » and « update » + tests

--- __tests__/notifications.test.js
+++ __tests__/notifications.test.js
@@ -1,66 +1,40 @@
 describe('chrome.notifications', () => {
   test('create', done => {
-    const notificationId = null;
-    const options = {
-      type: 'basic',
-      title: 'Title',
-      message: 'My message',
-      icons: 'image.png',
-    };
+    const options = { type: 'basic' };
     const callback = jest.fn(() => done());
 
     expect(jest.isMockFunction(chrome.notifications.create)).toBe(true);
 
-    chrome.notifications.create(options);
+    chrome.notifications.create(options, callback);
     expect(chrome.notifications.create).toHaveBeenCalledTimes(1);
-    expect(chrome.notifications.create).toHaveBeenLastCalledWith(options);
-    expect(callback).not.toHaveBeenCalled();
-
-    chrome.notifications.create(notificationId, options);
-    expect(chrome.notifications.create).toHaveBeenCalledTimes(2);
     expect(chrome.notifications.create).toHaveBeenLastCalledWith(
-      notificationId,
       options,
-    );
-    expect(callback).not.toHaveBeenCalled();
-
-    chrome.notifications.create(notificationId, options, callback);
-    expect(chrome.notifications.create).toHaveBeenCalledTimes(3);
-    expect(chrome.notifications.create).toHaveBeenLastCalledWith(
-      notificationId,
-      options,
-      callback,
+      callback
     );
     expect(callback).toHaveBeenLastCalledWith('generated-id');
 
-    chrome.notifications.create('my-notification-id', options, callback);
-    expect(chrome.notifications.create).toHaveBeenCalledTimes(4);
+    chrome.notifications.create('id', options, callback);
+    expect(chrome.notifications.create).toHaveBeenCalledTimes(2);
     expect(chrome.notifications.create).toHaveBeenLastCalledWith(
-      'my-notification-id',
+      'id',
       options,
-      callback,
+      callback
     );
-    expect(callback).toHaveBeenLastCalledWith('my-notification-id');
+    expect(callback).toHaveBeenLastCalledWith('id');
   });
 
   test('update', done => {
-    // Same behavior than « create » method, so I will write a very simple test
-    const notificationId = null;
-    const options = {
-      type: 'basic',
-      title: 'Title',
-      message: 'My message',
-      icons: 'image.png',
-    };
+    const options = { type: 'basic' };
     const callback = jest.fn(() => done());
 
-    chrome.notifications.update(notificationId, options, callback);
+    expect(jest.isMockFunction(chrome.notifications.update)).toBe(true);
+    chrome.notifications.update('id', options, callback);
     expect(chrome.notifications.update).toHaveBeenCalledTimes(1);
     expect(chrome.notifications.update).toHaveBeenLastCalledWith(
-      notificationId,
+      'id',
       options,
-      callback,
+      callback
     );
-    expect(callback).toHaveBeenLastCalledWith('generated-id');
+    expect(callback).toHaveBeenLastCalledWith(true);
   });
 });
--- src/notifications.js
+++ src/notifications.js
@@ -1,17 +1,10 @@
-const createOrUpdate = (notificationId, options, cb) => {
-  if (arguments.length === 1) {
-    // only options is specified
-    options = notificationId;
-    notificationId = undefined;
-  } else if (arguments.length === 2 && typeof notificationId === 'object') {
-    // options & cb are specified
-    options = notificationId;
-    cb = options;
-    notificationId = undefined;
-  }
-
+const create = (notificationId, options, cb) => {
   if (typeof notificationId !== 'string') {
     notificationId = 'generated-id';
+  }
+
+  if (typeof options === 'function') {
+    cb = options;
   }
 
   if (cb !== undefined) {
@@ -20,8 +13,15 @@ const createOrUpdate = (notificationId, options, cb) => {
 
   return Promise.resolve(notificationId);
 };
+const update = (notificationId, options, cb) => {
+  if (cb !== undefined) {
+    return cb(true);
+  }
+
+  return Promise.resolve(true);
+};
 
 export const notifications = {
-  create: jest.fn(createOrUpdate),
-  update: jest.fn(createOrUpdate),
+  create: jest.fn(create),
+  update: jest.fn(update),
 };
