c887e5bc5e00d9ff4266d1b6c17bc70df755f879
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-07-22 20:24:01 +0200

    Add mock for browser.i18n

---
+++ __tests__/i18n.test.js
@@ -0,0 +1,13 @@
+describe('browser.i18n', () => {
+  test('getMessage', () => {
+    return expect(
+      browser.i18n.getMessage('keyToTranslate')
+    ).toBe('Translated<keyToTranslate>');
+  });
+
+  test('getUILanguage', () => {
+    return expect(
+      browser.i18n.getUILanguage()
+    ).toBe('en');
+  });
+});
